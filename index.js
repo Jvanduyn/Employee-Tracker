7b092f8cf1f557f5186ab8b84c013f89480f2d89
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-01-12 18:31:17 -0500

    trying to get the update role going

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('console.table')
+const process = require('process')
 // Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        // MySQL username,
         user: 'root',
-        // TODO: Add MySQL password here
         password: 'Beagle150!',
         database: 'tracker_db'
     },
     console.log(`Connected to the tracker_db database.`)
 );
 
+// initial function that will give you options as to what you want to do, from there it will run appropriate function
 const init = () => {
     return inquirer.prompt([
         {
@@ -50,6 +50,7 @@ const init = () => {
     })
 }
 
+// function to show table of employees
 function viewAllEmployees() {
     db.query('SELECT * FROM employee', function (err, results) {
         console.table(results);
@@ -57,6 +58,92 @@ function viewAllEmployees() {
     });
 }
 
+// This function is adding an employee to the database by asking the questions below
+function addEmployee() {
+    db.query('SELECT * FROM role', function (err, results) {
+        const roleArr = results.map(({ department_id, title }) => ({
+            name: title,
+            value: department_id
+        }))
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'What is the employees first name?',
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'What is the employees last name?',
+            },
+            {
+                type: 'list',
+                name: 'title',
+                message: 'What is the employees role?',
+                choices: roleArr,
+            },
+        ]).then((answers) => {
+            const sql = `INSERT INTO employee (first_name, last_name, role_id)
+            VALUES (?,?,?)`;
+            const params = [answers.first_name, answers.last_name, answers.role_id]
+
+            db.query(sql, params, (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return;
+                }
+                init()
+            })
+        })
+
+    });
+}
+
+// need to store the list of employees into an array
+// ask which employee they would like to update
+// need to store role names into an array
+// ask what role they would like to change it to
+function updateEmployeeRole() {
+    db.query('SELECT * FROM employee', function (err, results) {
+        const employeeArr = results.map(({ id, title }) => ({
+            name: title,
+            value: title
+        }))
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'What is the employees first name?',
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'What is the employees last name?',
+            },
+            {
+                type: 'list',
+                name: 'title',
+                message: 'What is the employees name you would like to update?',
+                choices: employeeArr,
+            },
+        ]).then((answers) => {
+            const sql = `INSERT INTO employee (first_name, last_name, role_id)
+            VALUES (?,?,?)`;
+            const params = [answers.first_name, answers.last_name, answers.role_id]
+
+            db.query(sql, params, (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return;
+                }
+                init()
+            })
+        })
+
+    });
+}
+
+// function to show table of all roles
 function viewAllRoles() {
     db.query('SELECT * FROM role', function (err, results) {
         console.table(results);
@@ -64,46 +151,7 @@ function viewAllRoles() {
     });
 }
 
-function viewAllDepartments() {
-    db.query('SELECT * FROM department', function (err, results) {
-        console.table(results);
-        init()
-    });
-}
-
-function addDepartment() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the department?',
-        }
-    ]).then((answers) => {
-        const sql = `INSERT INTO department (department_name)
-        VALUES (?)`;
-        const params = [answers.name];
-
-        db.query(sql, params, (err, result) => {
-            if (err) {
-                res.status(400).json({ error: err.message });
-                return;
-            }
-            init()
-        })
-    })
-}
-
-function viewAllDepartments() {
-    db.query('SELECT * FROM department', function (err, results) {
-        console.table(results);
-        init()
-    });
-}
-
-// adding a new role
-// prompt to ask input role, input salary, list departments
-// to list departments, you would need a dynamic list of all the departments if someone added one
-// user would select criteria and it would add that information to the role table
+// function to add role
 function addRole() {
     db.query('SELECT * FROM department', function (err, results) {
         const departmentArr = results.map(({ id, department_name }) => ({
@@ -144,4 +192,41 @@ function addRole() {
     });
 }
 
+// function to view department table
+function viewAllDepartments() {
+    db.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
+        init()
+    });
+}
+
+// function to add a deparment to table
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the department?',
+        }
+    ]).then((answers) => {
+        const sql = `INSERT INTO department (department_name)
+        VALUES (?)`;
+        const params = [answers.name];
+
+        db.query(sql, params, (err, result) => {
+            if (err) {
+                res.status(400).json({ error: err.message });
+                return;
+            }
+            init()
+        })
+    })
+}
+
+function quit() {
+    console.log('Goodbye')
+    process.exit()
+}
+
+// calling initial function to run the initial inquirer
 init();
